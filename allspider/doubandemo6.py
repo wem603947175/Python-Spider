9d37995b1ea5e3d25a9d624cc34b5d2461953b2c
wem <603947175@qq.com>
2018-11-28 17:12:55 +0800

    爬取豆瓣250-单线程爬虫

---
+++ allspider/doubandemo6.py
@@ -0,0 +1,49 @@
+import requests
+import json
+import time
+from lxml import etree
+from threading import Thread
+from queue import Queue
+
+
+
+def spider(url):
+    print(url)
+    response = requests.get(url=url)
+    content = response.content
+    content = content.decode('utf-8')
+
+    '''数据处理'''
+    html = etree.HTML(content)
+    item_list = html.xpath('//div[@class="item"]')
+    for item in item_list:
+        movie = {}
+        movie['编号'] = item.xpath('./div[1]/em/text()')[0]
+        movie['名称'] = item.xpath('./div[2]//a/span[1]/text()')[0]
+        movie['评分'] = item.xpath('./div[2]//span[@class="rating_num"]/text()')[0]
+
+        movie_list.put(movie)
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    # 存储数据
+    movie_list = Queue()
+
+    '''发送请求'''
+    base_url = 'https://movie.douban.com/top250?filter=&start='
+
+    for i in range(10):
+        url = base_url + str(i * 25)
+        spider(url)
+
+    end_time = time.time()
+
+    while not movie_list.empty():
+        print(movie_list.get())
+
+
+
+    print('爬取时间总结：%s' % (end_time - start_time))
+
+
